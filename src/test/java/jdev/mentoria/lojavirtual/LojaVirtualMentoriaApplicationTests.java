2bd7c36e827061bc6797800cc74ff19b37f6069a
ygordaniel_pc <ygordaniel_pc@YGORDANIEL-PC>
2022-01-19 19:34:31 -0300

    Aula 3.2 Criando End-Point e Teste com Junit

--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,19 +1,24 @@
 package jdev.mentoria.lojavirtual;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import jdev.mentoria.lojavirtual.controller.AcessoController;
 import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
+import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
-public class LojaVirtualMentoriaApplicationTests {
+public class LojaVirtualMentoriaApplicationTests extends TestCase {
 
-
-	
 	@Autowired
 	private AcessoController acessoController;
+
+	@Autowired
+	private AcessoRepository acessoRepository;
 
 	@Test
 	public void testCadastraAcesso() {
@@ -22,7 +27,41 @@ public class LojaVirtualMentoriaApplicationTests {
 
 		acesso.setDescricao("ROLE_ADMIN");
 
-		acessoController.salvarAcesso(acesso);
+		assertEquals(true, acesso.getId() == null);
+
+		/* Gravou no banco de dados */
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		assertEquals(true, acesso.getId() > 0);
+
+		/* Validar dados salvos da forma correta */
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+
+		/* Teste de carregamento */
+		Acesso acessoCarredado = acessoRepository.findById(acesso.getId()).get();
+
+		assertEquals(acesso.getId(), acessoCarredado.getId());
+
+		/* Teste de delete */
+		acessoRepository.deleteById(acessoCarredado.getId());
+
+		Acesso acessoDelete = acessoRepository.findById(acessoCarredado.getId()).orElse(null);
+
+		assertEquals(true, acessoDelete == null);
+		
+		/* Teste de query */
+
+		acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_ALUNO");
+
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+
+		assertEquals(1, acessos.size());
+
+		acessoRepository.deleteById(acesso.getId());
 
 	}
 
